611dab222476d507a90a77d72d5f79ad72029bb2
ritesh848288 <139844875+ritesh848288@users.noreply.github.com>
2025-09-02 06:04:27 +0530

    Create First_Assiment.py
    This is frist assignment that take input and give a basic mathematical solution like + - * /

---
+++ First_Assiment.py
@@ -0,0 +1,19 @@
+# Task 1: Perform Basic Mathematical Operations
+
+# Take two numbers as input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+# Display results
+print("\nResults of Basic Mathematical Operations:")
+print( 'Addition :', addition)
+print('Subtraction:',subtraction)
+print('Multiplication:',multiplication)
+print('Division:',division)
